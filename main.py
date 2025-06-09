97dd1b8047a927cd29c8dea5b9a3629e101537f0
Gabriel Rizzi <75540693+Rizzi-dev@users.noreply.github.com>
2025-06-08 23:21:51 -0300

    remove a lot archives and implement dfs search

--- main.py
+++ main.py
@@ -4,29 +4,31 @@ import time
 
 ## importa classes
 from vs.environment import Env
-from explorer import Explorer
+from explorer_dfs import Explorer
 from rescuer import Rescuer
 
 def main(data_folder_name):
-   
+  
     # Set the path to config files and data files for the environment
     current_folder = os.path.abspath(os.getcwd())
     data_folder = os.path.abspath(os.path.join(current_folder, data_folder_name))
 
-    
     # Instantiate the environment
     env = Env(data_folder)
     
     # config files for the agents
-    rescuer_file = os.path.join(data_folder, "rescuer_config.txt")
-    explorer_file = os.path.join(data_folder, "explorer_config.txt")
+    rescuer_file = os.path.join(data_folder, "rescuer_1_config.txt")
+    explorer_file = os.path.join(data_folder, "explorer_1_config.txt")
     
     # Instantiate agents rescuer and explorer
     resc = Rescuer(env, rescuer_file)
 
     # Explorer needs to know rescuer to send the map
     # that's why rescuer is instatiated before
-    exp = Explorer(env, explorer_file, resc)
+    exp = Explorer(env, explorer_file, resc, 0)
+    exp2 = Explorer(env, explorer_file, resc, 2)
+    exp3 = Explorer(env, explorer_file, resc, 4)
+    exp4 = Explorer(env, explorer_file, resc, 6)
 
     # Run the environment simulator
     env.run()
@@ -39,6 +41,6 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         data_folder_name = sys.argv[1]
     else:
-        data_folder_name = os.path.join("datasets", "data_10v_12x12")
+        data_folder_name = os.path.join("datasets", "data_430v_94x94")
         
     main(data_folder_name)
